bad6492d50310ca2605db86c1cac332a3cc750ef
Jonathan Clark <jonny@Jonathans-MacBook-Pro.local>
2020-10-01 22:15:48 -0700

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Homescreen from "./screens/Homescreen";
+import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
-  return <>Welcome to Offroad Swaps</>;
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={Homescreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/screens/ProductScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating/Rating";
+import products from "../products";
+
+const ProductScreen = ({ match }) => {
+  const product = products.find((product) => product._id === match.params.id); //Matches to the product/:id of the URL
+  return <div></div>;
+};
+
+export default ProductScreen;
