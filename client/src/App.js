99ca79de696809de3aa66c35fa8ebffa34f68a57
Jonathan Clark <jonny@Jonathans-MacBook-Pro.local>
2020-10-12 22:43:50 -0700

    finished user auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Homescreen from "./screens/Homescreen";
-import ProductScreen from "./screens/ProductScreen";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import CartScreen from "./screens/CartScreen.jsx";
+import React from "react"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import ProductScreen from "./screens/ProductScreen"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import CartScreen from "./screens/CartScreen.jsx"
+import Homescreen from "./screens/Homescreen"
+import LoginScreen from "./screens/LoginScreen.js"
 
 const App = () => {
   return (
@@ -13,14 +14,15 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route exact path="/" component={Homescreen} />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route exact path="/" component={Homescreen} />
         </Container>
       </main>
       <Footer />
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- client/src/store.js
+++ client/src/store.js
@@ -1,29 +1,42 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
 import {
   productListReducer,
   productDetailsReducer,
-} from "./reducers/productReducers.js";
-import { cartReducer } from "./reducers/cartReducers.js";
+} from "./reducers/productReducers.js"
+import { cartReducer } from "./reducers/cartReducers.js"
+import {
+  userLoginReducer,
+  userRegisterReducer,
+} from "./reducers/userReducers.js"
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
-});
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+})
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
-  : [];
+  : []
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null
 
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
-};
-const middleware = [thunk];
+  cart: {
+    cartItems: cartItemsFromStorage,
+    userLogin: { userInfo: userInfoFromStorage },
+  },
+}
+const middleware = [thunk]
 const store = createStore(
   reducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
-);
-export default store;
+)
+export default store
